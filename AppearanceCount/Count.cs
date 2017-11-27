66d1c8dae65919d2402f10016d4ebe4dd0df9d6c
orlindraganov <orlindraganov@gmail.com>
2017-11-27 18:43:59 +0200

    Done 04.AppearanceCount

---
+++ AppearanceCount/Count.cs
@@ -0,0 +1,34 @@
+﻿namespace AppearanceCount
+{
+    using System;
+    using System.Linq;
+
+    class Count
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            int[] arr = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountAppearance(arr, number));
+        }
+
+        static int CountAppearance(int[] searchableArray, int numberForSearching)
+        {
+            int counter = 0;
+
+            foreach (var currentNumber in searchableArray)
+            {
+                if (currentNumber == numberForSearching)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
