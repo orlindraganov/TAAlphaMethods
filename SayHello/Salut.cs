acb53fd065003f9ac4b000115497e14346106585
orlindraganov <orlindraganov@gmail.com>
2017-11-27 17:54:09 +0200

    Init, done task 1

---
+++ SayHello/Salut.cs
@@ -0,0 +1,21 @@
+﻿namespace SayHello
+{
+    using System;
+
+    class Salut
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+
+            SayHello(name);
+        }
+
+        static void SayHello(string name)
+        {
+            var greeting = string.Format("Hello, {0}!",name);
+
+            Console.WriteLine(greeting);
+        }
+    }
+}
