e35f3b4655912a514014aac9c87804766c85f133
orlindraganov <orlindraganov@gmail.com>
2017-11-27 18:33:59 +0200

    Added EnglishDigit with enum

---
+++ EnglishDigit/Digit.cs
@@ -0,0 +1,49 @@
+﻿namespace EnglishDigit
+{
+    using System;
+
+    class Digit
+    {
+        private enum Digits
+        {
+            zero,
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine
+        };
+
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ParseLastDigitToString(number));
+        }
+
+        static string ParseLastDigitToString(int number)
+        {
+            if (0 > number ^ number > 9)
+            {
+                number = ReturnLastDigit(number);
+            }
+
+            string digitAsString = Enum.GetName(typeof(Digits), number);
+
+            return digitAsString;
+        }
+
+        static int ReturnLastDigit(int number)
+        {
+            int result;
+
+            result = number % 10;
+
+            return result;
+        }
+    }
+}
