7e946f4025664c7b42625ba1a71bea42532eb4d3
orlindraganov <orlindraganov@gmail.com>
2017-11-27 18:10:42 +0200

    Done task 2.GetLargest. Should work for n ints

---
+++ GetLargest/Largest.cs
@@ -0,0 +1,43 @@
+﻿namespace GetLargest
+{
+    using System;
+    using System.Linq;
+
+    class Largest
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] separator = { ' ' };
+
+            int[] numbers = input.Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+
+            Console.WriteLine(GetMax(numbers));
+        }
+
+        static int GetMax(int[] numbers)
+        {
+            int max = int.MinValue;
+
+            foreach (var number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            return max;
+        }
+
+        static int GetMax(int firstInt, int secondInt)
+        {
+            if (firstInt > secondInt)
+            {
+                return firstInt;
+            }
+
+            return secondInt;
+        }
+    }
+}
