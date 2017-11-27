d6c235943921c62efd7e5c78446cc6d01bc39b74
orlindraganov <orlindraganov@gmail.com>
2017-11-27 19:57:52 +0200

    Done tasks 05. & 06.

---
+++ FirstLargerThanNeighbours/FirstLarger.cs
@@ -0,0 +1,34 @@
+﻿namespace FirstLargerThanNeighbours
+{
+    using System;
+    using System.Linq;
+
+    class FirstLarger
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            int[] arr = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+
+            Console.WriteLine(IndexOfFirstLarger(arr));
+        }
+
+        static int IndexOfFirstLarger(int[] arr)
+        {
+            var index = -1;
+
+            //Check conditions
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ LargerThanNeighbours/Larger.cs
@@ -0,0 +1,37 @@
+﻿namespace LargerThanNeighbours
+{
+    using System;
+    using System.Linq;
+
+    class Larger
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            int[] arr = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+
+            Console.WriteLine(CountLargerThanNeighbours(arr));
+        }
+
+        static int CountLargerThanNeighbours(int[] arr)
+        {
+            var counter = 0;
+
+            //Check conditions
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i-1] && arr[i]>arr[i+1])
+                {
+                    counter++;
+
+                    //Next one can't be bigger than this one, 
+                    //so we hop to the next i :)
+                    i++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
